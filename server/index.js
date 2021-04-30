b2869e343fce78b5fe20e8dcc82c03a762362801
QwertyAswal <aswals8991@gmail.com>
2021-04-30 16:11:32 +0530

    Placehorder code for front end added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
+const aboutRouter = require('./routes/about')
+const homeRouter = require('./routes/home')
+const loginRouter = require('./routes/login')
+const projectsRouter = require('./routes/projects')
+const signupRouter = require('./routes/signup')
+
 const app = express()
 const port = process.env.PORT
 
@@ -10,15 +16,18 @@ const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.use(express.static(publicPath))
+express.use(express.json())
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
+app.use(aboutRouter)
+app.use(homeRouter)
+app.use(loginRouter)
+app.use(projectsRouter)
+app.use(signupRouter)
 
-app.get('/', (req, res) => {
-    res.render('home', { title: 'Homer' })
-})
 
 app.listen(port, () => {
     console.log(`Server listening at port:- ${port}`)
